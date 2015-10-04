d3c438ff86494736737dbf9e0e85eb953305b80f
unknown <nicolapanozzo64@gmail.com>
2015-10-04 19:47:43 +0100

    Distance Unit Test

---
+++ InviteTests/InviteUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Invite;
+
+namespace InviteTests
+{
+    [TestClass]
+    public class InviteUnitTests
+    {
+        [TestMethod]
+        public void DistanceReturnsCorrectDistance()
+        {
+            double latitude1 = 0.929302344;
+            double longitude1 = -0.108879486;
+            double latitude2 = 0.930927181;
+            double longitude2 = -0.109244654;
+
+            double expectedDistance = 10.444825879238;
+
+            GPSPoint point1 = new GPSPoint(latitude1, longitude1);
+            GPSPoint point2 = new GPSPoint(latitude2, longitude2);
+
+            double actualDistance = point1.Distance(point2);
+
+            Assert.AreEqual(expectedDistance, actualDistance, 0.000001, "Distance not calculated correctly");
+
+        }
+    }
+}
--- InviteTests/Properties/AssemblyInfo.cs
+++ InviteTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Invite")]
+[assembly: AssemblyTitle("InviteTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Generali PanEurope")]
-[assembly: AssemblyProduct("Invite")]
-[assembly: AssemblyCopyright("Copyright © Generali PanEurope 2015")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("InviteTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18fb1995-ee0d-4e3d-a59d-d5f3552586ba")]
+[assembly: Guid("f48b01d3-9850-4696-8cc6-887ca937612b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("InviteTests")]
